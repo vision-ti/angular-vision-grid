041cdac26c1555da3f43b6fac5154bc42a13a7c6
Felipe Leonhardt <felipe.ul@gmail.com>
2014-10-13 08:47:21 -0300

    Code error resolved

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
                 options: { livereload: true },
                 files: ['demo/**/*']
             }
-        },
+        }
     });
 
     /*config.set("concat.dist", {
